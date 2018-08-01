564f3e2eb2a5b0ec623a1cf360911f99239cc7cb
Dylan <dylan@Dylans-MacBook-Pro.local>
2018-08-01 10:46:55 -0400

    Emit message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Dylan',
-    text: 'Hey, test'
-  });
 });
 
 socket.on('disconnect', function () {
